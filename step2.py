6cdd0afdc0ce67a1d9327c24e5634aa9d5f6ec22
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-09-20 13:31:18 +0800

    Create step2.py

---
+++ step2.py
@@ -0,0 +1,22 @@
+# Define the class
+class className
+
+    # Define a method
+    def method1Name
+        ...blocks of code
+
+    # Define another method
+    def method2Name
+        ...blocks of code
+
+    # Define yet another method
+    def method3Name
+        ...blocks of code
+
+# Instantiate the class
+myClass = className()
+
+# Call the instantiation and associated methods
+myClass.method1Name()
+myClass.method2Name()
+myClass.method3Name()

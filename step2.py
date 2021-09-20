8fe69ba15241021ecf623af56dc05d0014f78998
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-09-20 13:35:40 +0800

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,22 +1,5 @@
-# Define the class
-class className
-
-    # Define a method
-    def method1Name
-        ...blocks of code
-
-    # Define another method
-    def method2Name
-        ...blocks of code
-
-    # Define yet another method
-    def method3Name
-        ...blocks of code
-
-# Instantiate the class
-myClass = className()
-
-# Call the instantiation and associated methods
-myClass.method1Name()
-myClass.method2Name()
-myClass.method3Name()
+def myCity(city):
+     print("I live in " + city + ".")
+myCity("Austin")
+myCity("Tokyo")
+myCity("Salzburg")

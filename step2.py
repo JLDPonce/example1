5fd981aeb4542248890ba17ebe6067b5e366e347
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-09-20 13:43:05 +0800

    Update step2.py

--- step2.py
+++ step2.py
@@ -15,3 +15,21 @@ loc = Location("Your_Name", "Your_Country")
 print(loc.name)
 print(loc.country)
 print(type(loc))
+
+
+class Location:
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+        self.country = country
+
+    |<--Your cursor should be here
+def myLocation(self):
+        print("Hi, my name is " + self.name + " and I live in " + self.country + ".")
+
+          
+          
+# First instantiation of the class Location 
+loc1 = Location("Tomas", "Portugal")
+# Call a method from the instantiated class
+loc1.myLocation()

bf36ddaa97e9bbda7e54726b6d15a4a6e5799a8c
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-09-20 13:39:03 +0800

    Update step2.py

--- step2.py
+++ step2.py
@@ -3,3 +3,15 @@ def myCity(city):
 myCity("Austin")
 myCity("Tokyo")
 myCity("Salzburg")
+
+class Location:
+    |<-- cursor should now be here
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+|<-- cursor should now be here
+loc = Location("Your_Name", "Your_Country")
+print(loc.name)
+print(loc.country)
+print(type(loc))

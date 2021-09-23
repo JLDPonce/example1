b7a1b84dc2ef27d30662f51ec761f0e71cfe30b7
JLDPonce <89880910+JLDPonce@users.noreply.github.com>
2021-09-23 22:18:40 +0800

    Update step2.py

--- step2.py
+++ step2.py
@@ -360,3 +360,6 @@ import yaml
 with open('myfile.json','r') as json_file:
     ourjson = json.load(json_file)
 print(ourjson)
+print("The access token is: {}".format(ourjson['access_token']))
+print("The token expires in {} seconds.".format(ourjson['expires_in']))
+
